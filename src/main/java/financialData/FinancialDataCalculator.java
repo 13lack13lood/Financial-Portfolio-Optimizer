34cf6833b734253967d5cca85d429c0c951cbb2f
Harry Zhu <harryzhu155@gmail.com>
2024-02-09 17:58:20 -0500

    add more math

--- src/main/java/financialData/FinancialDataCalculator.java
+++ src/main/java/financialData/FinancialDataCalculator.java
@@ -1,5 +1,34 @@
 package financialData;
 
-public class FinancialDataCalculator {
+import util.MathUtil;
 
+public class FinancialDataCalculator {
+	
+	private String[] tickers;
+	private double[][] historicalReturns;
+	private double[][] covarience;
+	private double[] mean;
+	private double[] standardDeviation;
+	
+	public FinancialDataCalculator(String[] tickers) {
+		this.tickers = tickers;
+
+		historicalReturns = FinancialData.getVariousHistoricalReturns(tickers);
+		
+		covarience = MathUtil.calculateCovarience(historicalReturns);
+		mean = MathUtil.calculateMean(historicalReturns);
+		standardDeviation = MathUtil.calculateStandardDeviation(historicalReturns);
+	}
+	
+	
+	public double calculateExpectedReturns(double[] portfolio) {
+		double sum = 0;
+		
+		for(int i = 0; i < portfolio.length; i++) {
+			sum += portfolio[i] * mean[i];
+		}
+		
+		return sum;
+	}
+	
 }
--- src/main/java/util/MathUtil.java
+++ src/main/java/util/MathUtil.java
@@ -27,7 +27,7 @@ public class MathUtil {
 		return array;
 	}
 	
-	private static double calculateAverage(double[] array) {
+	private static double calculateMean(double[] array) {
 		double output = 0;
 		
 		for(double i : array) {
@@ -37,14 +37,24 @@ public class MathUtil {
 		return output / array.length;
 	}
 	
+	public static double[] calculateMean(double[][] array) {
+		double[] output = new double[array.length];
+		
+		for(int i = 0; i < output.length; i++) {
+			output[i] = calculateMean(array[i]);
+		}
+		
+		return output;
+	}
+	
 	private static double calculateCovarience(double[] first, double[] second) {
 		double output = 0;
 		
-		double firstAverage = calculateAverage(first);
-		double secondAverage = calculateAverage(second);
+		double firstMean = calculateMean(first);
+		double secondMean = calculateMean(second);
 		
 		for(int i = 0; i < Math.min(first.length, second.length); i++) {
-			output += (first[i] - firstAverage) * (second[i] - secondAverage);
+			output += (first[i] - firstMean) * (second[i] - secondMean);
 		}
 		
 		return output / (Math.min(first.length, second.length) - 1);
@@ -61,6 +71,30 @@ public class MathUtil {
 		
 		for(int i = 0; i < output.length; i++) {
 			output[i][i] = 0;
+		}
+		
+		return output;
+	}
+	
+	private static double standardDeviation(double[] array) {
+		double output = 0;
+		
+		double mean = calculateMean(array);
+		
+		for(double i : array) {
+			output += Math.pow(i - mean, 2);
+		}
+		
+		output /= array.length;
+		
+		return Math.sqrt(output);
+	}
+	
+	public static double[] calculateStandardDeviation(double[][] array) {
+		double[] output = new double[array.length];
+		
+		for(int i = 0; i < array.length; i++) {
+			output[i] = standardDeviation(array[i]);
 		}
 		
 		return output;
